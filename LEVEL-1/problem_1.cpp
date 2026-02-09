7f56f5d7a6622da9e06bc59e2ded5cc2478466a6
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-02-09 21:17:08 +0530

    Added codes for problem_1.cpp

--- LEVEL-1/problem_1.cpp
+++ LEVEL-1/problem_1.cpp
@@ -1,1 +1,13 @@
 #include<iostream>
+#include<string>
+using namespace std;
+//print your full name 100 times
+
+main(){
+    string name;
+    cout<<"Enter your full name: ";
+    getline(cin, name);
+    for (int i = 0; i < 100; i++){
+        cout<<name<<endl;
+    }
+}
