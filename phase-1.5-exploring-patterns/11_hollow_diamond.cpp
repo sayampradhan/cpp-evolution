fd31ff73a69407091e2ca8d70c8dfdb4cd41c9d6
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-02-17 17:40:58 +0530

    Update 11_hollow_diamond.cpp

--- phase-1.5-exploring-patterns/11_hollow_diamond.cpp
+++ phase-1.5-exploring-patterns/11_hollow_diamond.cpp
@@ -2,10 +2,16 @@
 #include<limits>
 using namespace std;
 
-void hollow_diamond(){
+void hollow_diamond(int n){
+    for (int i = 1; i <= n; i++){
+        for (int j = 1; j <= n; j++){
+            cout<<"* ";
+        }
+        cout<<" ";
+    }
 
 }
 
 main(){
-    
+    hollow_diamond(5);
 }
