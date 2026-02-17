597b4aa9ba2baf01bcda2a40077e5aad691471a3
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-02-17 17:44:23 +0530

    updated files

--- phase-1.5-exploring-patterns/11_hollow_diamond.cpp
+++ phase-1.5-exploring-patterns/11_hollow_diamond.cpp
@@ -4,10 +4,13 @@ using namespace std;
 
 void hollow_diamond(int n){
     for (int i = 1; i <= n; i++){
-        for (int j = 1; j <= n; j++){
+        for (int k = 1; k <= n; k++){
+            cout<<" ";
+        }
+        for (int j = 1; j <= i; j++){
             cout<<"* ";
         }
-        cout<<" ";
+        cout<<endl;
     }
 
 }
