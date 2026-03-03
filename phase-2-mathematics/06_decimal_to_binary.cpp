8b5c529e6f26f777ac5feb4893bf7704e69e944b
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-03 22:59:55 +0530

    added problem decimal to binary conversion

---
+++ phase-2-mathematics/06_decimal_to_binary.cpp
@@ -0,0 +1,6 @@
+#include<iostream>
+using namespace std;
+
+// Decimal to binary conversion without using builtin functions.
+// I used recursion to actually calculate it.
+void dec_to_bin(int num){
