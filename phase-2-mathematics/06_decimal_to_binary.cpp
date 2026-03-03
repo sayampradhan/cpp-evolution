94f562d66a7863ef9e1b8e9cde69dac81b9fdf4d
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-03 23:00:22 +0530

    Update 06_decimal_to_binary.cpp

--- phase-2-mathematics/06_decimal_to_binary.cpp
+++ phase-2-mathematics/06_decimal_to_binary.cpp
@@ -4,3 +4,13 @@ using namespace std;
 // Decimal to binary conversion without using builtin functions.
 // I used recursion to actually calculate it.
 void dec_to_bin(int num){
+    if (num == 0){
+        return;
+    }
+
+    dec_to_bin(num / 2);
+
+    cout << num % 2;
+
+}
+
