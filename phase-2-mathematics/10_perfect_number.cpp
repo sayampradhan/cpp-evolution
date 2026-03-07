04d3fb14940ca6949a4c31d0dccd7f7bad3eba69
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-07 19:43:54 +0530

    updated

--- phase-2-mathematics/10_perfect_number.cpp
+++ phase-2-mathematics/10_perfect_number.cpp
@@ -25,7 +25,7 @@ main(){
     int temp = 0;
     for (int i = 0; i <= n; i++){
         if (n % i == 0){
-            int temp = i;
+            temp = i;
             divisors.push_back(temp);
         }
     }
