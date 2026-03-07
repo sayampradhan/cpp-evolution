61561ef85eaf2c7cd7f98256eb70cc6868a03988
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-07 19:48:46 +0530

    updated

--- phase-2-mathematics/10_perfect_number.cpp
+++ phase-2-mathematics/10_perfect_number.cpp
@@ -23,23 +23,19 @@ main(){
     vector<int> divisors = {};
     int sum = 0;
     int temp = 0;
-    for (int i = 0; i <= n; i++){
+    for (int i = 0; i <= n/2; i++){
         if (n % i == 0){
-            temp = i;
-            divisors.push_back(temp);
+            divisors.push_back(i);
         }
     }
-    for (int element: divisors){
-        cout<<"Element: "<<element<<" ";
-        sum+=element;
+    for (int element : divisors) {
+        sum += element;
     }
 
-    if(n == sum){
-        cout<<"Perfect number";
-    }
-
-    else{
-        cout<<"NOT a Perfect number";
+    if (n == sum && n != 0) {
+        cout << n << " is a Perfect number" << endl;
+    } else {
+        cout << n << " is NOT a Perfect number" << endl;
     }
 
 }
