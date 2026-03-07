2bf16ffb2abcd76d280f630e23b05ace47616826
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-07 19:35:10 +0530

    Update 10_perfect_number.cpp

--- phase-2-mathematics/10_perfect_number.cpp
+++ phase-2-mathematics/10_perfect_number.cpp
@@ -3,6 +3,15 @@
 #include<algorithm>
 using namespace std;
 
+// Program to check if a number is a perfect number or not.
+/*
+    TO CHECK IT:
+    1. Take the input of a number.
+    2. Find all the divisors of a number.
+    3. Store them into an vector array.
+
+
+*/
 void perfect_number(int n){
     vector<int> divisors = {};
 
