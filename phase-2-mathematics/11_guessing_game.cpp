e5516c37d9791c8730fea9fb2cdc5bbb214c6258
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-08 13:28:53 +0530

    Update 11_guessing_game.cpp

--- phase-2-mathematics/11_guessing_game.cpp
+++ phase-2-mathematics/11_guessing_game.cpp
@@ -2,10 +2,17 @@
 #include<random>
 using namespace std;
 
+/*
+BUILDING A GUESSING GAME
+
+- We let the machine generate a random number.
+*/
+
+
 int random_number(int start, int end){
 
 }
 
 main(){
-    
+
 }
