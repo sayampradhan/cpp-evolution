366ec0c09a76835134225bff17349af4ecbcf27e
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-08 19:41:40 +0530

    Update 01_second_largest_element.cpp

--- phase-3-data-structures/01_second_largest_element.cpp
+++ phase-3-data-structures/01_second_largest_element.cpp
@@ -7,7 +7,7 @@ FOUR ALGORITHMS used to solve this problem are:
     1. Single-Pass Approach
     2. Two-Pass Approach
     3. Sorting Approach
-    4. Tournament Appraoch
+    4. Tournament Approach
 
 
 */
