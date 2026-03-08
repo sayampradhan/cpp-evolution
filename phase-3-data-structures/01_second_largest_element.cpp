01708ac43006e301c79db1bd7fe41dcf23cd5942
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-08 19:48:00 +0530

    Update 01_second_largest_element.cpp

--- phase-3-data-structures/01_second_largest_element.cpp
+++ phase-3-data-structures/01_second_largest_element.cpp
@@ -1,4 +1,5 @@
 #include<iostream>
+#include<iterator>
 using namespace std;
 
 // Finding the second largest number in an array
@@ -9,6 +10,22 @@ FOUR ALGORITHMS used to solve this problem are:
     3. Sorting Approach
     4. Tournament Approach
 */
+
+void single_pass(int arr[]){
+    int max1, max2;
+    int length = std::size(arr);
+    for (int i = 0; i < length; i++){
+        if (arr[i]<arr[i+1]){
+            int max2 = arr[i];
+            int max1 = arr[i+1];
+        }
+        else{
+            int max2 = arr[i+1];
+            int max1 = arr[i];
+        }
+    }
+    cout<<"Second"
+}
 
 main(){
 
