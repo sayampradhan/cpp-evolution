6d88814a01f87da6584efc9a9e906f5252fb8f1e
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-08 13:32:37 +0530

    Update 11_guessing_game.cpp

--- phase-2-mathematics/11_guessing_game.cpp
+++ phase-2-mathematics/11_guessing_game.cpp
@@ -8,7 +8,7 @@ BUILDING A GUESSING GAME
 - We take the input from the user for range of the randomly generated number.
 - We let the machine generate a random number as per the user's preference.
 - We let the user input a number to guess it.
-    - If the number matches the input,
+    - If the number matches the random number,
         - we will say great. you guessed it right.
     - If the number is less than the random number,
         - we will say, the number is little greater.
