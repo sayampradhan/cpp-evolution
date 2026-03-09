f200f798b5cd108ab53d53072df2a4a3bfa6340b
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-09 22:19:21 +0530

    Update 02_remove_duplicates.cpp

--- phase-3-data-structures/02_remove_duplicates.cpp
+++ phase-3-data-structures/02_remove_duplicates.cpp
@@ -21,7 +21,8 @@ void sortArray(int *arr){
         int j = i - 1;
 
         while (j > 0 && arr[j] > key){
-            
+            arr[j + 1] = arr[j];
+            j = j - 1;
         }
     }
 }
