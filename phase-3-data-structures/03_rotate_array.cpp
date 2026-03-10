ed7e7281d88c0beb290ab374ff4b38e4bfc32a14
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-10 23:50:31 +0530

    updated code

--- phase-3-data-structures/03_rotate_array.cpp
+++ phase-3-data-structures/03_rotate_array.cpp
@@ -64,7 +64,7 @@ int main(){
 
     int size = sizeof(arr)/ sizeof(arr[0]);
 
-    cout<<"\nEnter no. of elements from the right to be rotated: ";
+    cout<<"\nEnter no. of elements to be rotated: ";
     cin>>k;
     if (!(k < size - 1)){
         cout<<"Enter again (k should be less than size) : ";
