f0ecd3ea62b86c15cde511144a3c5d78b4bc4d4c
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-10 10:11:50 +0530

    Update 03_rotate_array.cpp

--- phase-3-data-structures/03_rotate_array.cpp
+++ phase-3-data-structures/03_rotate_array.cpp
@@ -15,6 +15,10 @@ Example of Rotation by Right:
     2.  array = [10, 30, 90, 40, 6], k = 3
         Output: [90, 40, 6, 10, 30]
 
-
+Example of Rotation by Left:
+    1.  array = [1,2,3], k=1
+        Output: [2,3,1]
+    2.  array = [10,30,90,40,6], k = 3
+        Output: [40,6,10,30,90]
 
 */
