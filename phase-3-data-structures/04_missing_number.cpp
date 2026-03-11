5b8a61cee778e11d9ac20a4b10ea8ef4e2258f2b
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-11 22:36:50 +0530

    Update 04_missing_number.cpp

--- phase-3-data-structures/04_missing_number.cpp
+++ phase-3-data-structures/04_missing_number.cpp
@@ -1,5 +1,6 @@
 #include<iostream>
 #include<vector>
+#include<algorithm>
 using namespace std;
 
 /*
@@ -12,6 +13,7 @@ Output: 7
 
 // function to find the missing number
 int missing_num(vector<int> arr, int size){
+    int minNum = min_element(arr.begin(), arr.end());
     for (int i = 1; i <= size; i++){
         bool found = false;
         for (int j = 0; j < size - 1; j++){
