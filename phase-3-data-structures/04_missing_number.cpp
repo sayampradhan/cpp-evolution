4445578f74098f2d0148074fbbc5e65e902cae86
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-11 22:21:41 +0530

    Update 04_missing_number.cpp

--- phase-3-data-structures/04_missing_number.cpp
+++ phase-3-data-structures/04_missing_number.cpp
@@ -3,5 +3,15 @@
 using namespace std;
 
 /*
+To find the missing number in the array.
 
+Example: 
+Input: [1, 2, 3, 4, 5, 6, 8, 9]
+Output: 7
 */
+
+int main(){
+    vector <int> arr;
+    int n;
+    cout<<"Enter size: ";
+}
