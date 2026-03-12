8e2f10434d1a739d60d748921dffc3f2ea7a56cc
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-12 23:44:07 +0530

    Update 05_move_zeros_to_end.cpp

--- phase-3-data-structures/05_move_zeros_to_end.cpp
+++ phase-3-data-structures/05_move_zeros_to_end.cpp
@@ -6,12 +6,6 @@ using namespace std;
 // function to move zero to the end
 void moveZeros(vector<int> *arr, int size){
     int count = 0;
-    for (int i = 0; i < size; i++){
-        if(arr[i]==0){
-            count += 1;
-        }
-    }
-
 }
 
 main(){
