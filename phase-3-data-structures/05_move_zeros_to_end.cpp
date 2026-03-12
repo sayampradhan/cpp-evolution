5d39cec0f4d6b1630b8f644a40937a60d6bf6248
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-12 23:52:00 +0530

    Update 05_move_zeros_to_end.cpp

--- phase-3-data-structures/05_move_zeros_to_end.cpp
+++ phase-3-data-structures/05_move_zeros_to_end.cpp
@@ -17,12 +17,12 @@ main(){
     for (int i = 0; i < size; i ++){
         cout<<"Enter element at index "<<i<<" : ";
         cin>>temp;
-
+        arr.push_back(temp);
+        temp = 0;
     }
-
-
-
     int target = 0;
     int count = count(arr.begin(), arr.end(), target);
+    moveZeros(arr, arr.size(), count);
+
 }
 
