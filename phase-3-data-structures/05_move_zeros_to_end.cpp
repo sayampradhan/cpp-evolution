c9ce7e162830ebc222b1c7b6acb1710c937f712f
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-12 23:57:04 +0530

    Update 05_move_zeros_to_end.cpp

--- phase-3-data-structures/05_move_zeros_to_end.cpp
+++ phase-3-data-structures/05_move_zeros_to_end.cpp
@@ -3,8 +3,8 @@
 #include<algorithm>
 using namespace std;
 
-// function to move zero to the end
-void moveZeros(vector<int> *arr, int size, int count){
+
+void moveZeros(vector<int> &arr, int size, int count){
     int removeValue = 0;
     auto new_end = remove(arr.begin(), arr.end(), removeValue);
     arr.erase(new_end, arr.end());
@@ -19,21 +19,23 @@ void moveZeros(vector<int> *arr, int size, int count){
     }
 }
 
-main(){
+int main(){
     vector<int> arr;
     int size;
     int temp = 0;
+
     cout<<"Enter the length of the array: ";
     cin>>size;
-    for (int i = 0; i < size; i ++){
+
+    for (int i = 0; i < size; i++){
         cout<<"Enter element at index "<<i<<" : ";
         cin>>temp;
         arr.push_back(temp);
         temp = 0;
     }
+
     int target = 0;
-    int count = count(arr.begin(), arr.end(), target);
-    moveZeros(arr, arr.size(), count);
+    int zeroCount = count(arr.begin(), arr.end(), target);
 
+    moveZeros(arr, arr.size(), zeroCount);
 }
-
