47eeebcf60f159a1fd558a0d08fb24bb726e5376
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-13 20:51:44 +0530

    Update 07_merge_two_sorted_array.cpp

--- phase-3-data-structures/07_merge_two_sorted_array.cpp
+++ phase-3-data-structures/07_merge_two_sorted_array.cpp
@@ -3,4 +3,6 @@
 #include<algorithm>
 using namespace std;
 
-// 
+// Most efficient method to merge two sorted arrays is -- Two Pointer Method
+// Time complexity - O(n + m)
+// Space complexity - O(n + m)
