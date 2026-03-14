8fd1d002dff978cf6d1db064764814caa5efbf75
Sayam Pradhan <112542130+sayampradhan@users.noreply.github.com>
2026-03-14 20:21:23 +0530

    Create 08_binary_search.cpp

---
+++ phase-3-data-structures/08_binary_search.cpp
@@ -0,0 +1,26 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int binary_search(vector<int>& arr, int low, int mid, int high){
+
+}
+
+int main(){
+    vector<int> arr;
+    int size;
+    int low = 0;
+    int high;
+    int mid;
+
+    cout<<"Enter length of the array: ";
+    cin>>size;
+    cout<<endl<<endl;
+    for(int i = 0; i < size; i++){
+        int temp = 0;
+        cout<<"Enter element at index "<<i<<" : ";
+        cin>>temp;
+        arr.push_back(temp);
+    }
+
+}
