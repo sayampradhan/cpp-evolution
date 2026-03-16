11693709872de887e9b9ff22878dec596dd9feec
Sayam <112542130+sayampradhan@users.noreply.github.com>
2026-03-16 16:23:42 +0530

    updated code

--- phase-3-data-structures/10_buy-sell.cpp
+++ phase-3-data-structures/10_buy-sell.cpp
@@ -1,4 +1,22 @@
-#include<iostream>
+#include <iostream>
+#include <vector>
 using namespace std;
 
+int maxProfit(const vector<int>& prices) {
+    int res = 0;
+      
+    // Keep on adding the difference between
+    // adjacent when the prices a
+    for (int i = 1; i < prices.size(); i++) {
+        if (prices[i] > prices[i - 1]) 
+            res += prices[i] - prices[i - 1];
+    }
+                                              
+    return res;
+}
 
+int main() {
+            vector<int> prices = { 100, 180, 260, 310, 40, 535, 695 };
+            cout << maxProfit(prices) << endl;
+            return 0;
+}
