edaee7b69e5fe753f5a56f708fa476fd9afa717c
sayampradhan <sayampradhan07@outlook.com>
2026-03-29 10:15:00 +0530

    added problem 15

--- phase-3-data-structures/15_remove_spaces.cpp
+++ phase-3-data-structures/15_remove_spaces.cpp
@@ -1,3 +1,29 @@
-//
-// Created by sayam on 31/03/26.
-//
+#include <iostream>
+#include <string>
+using namespace std;
+
+string removeSpaces(string s) {
+    int n = s.length();
+
+    for (int i = 0; i < n; i++) {
+        if (s[i] == ' ') {
+
+            // shift all characters left
+            for (int j = i; j < n - 1; j++) {
+                s[j] = s[j + 1];
+            }
+            n--;
+            i--;
+        }
+    }
+
+    // return string up to new length
+    return s.substr(0, n);
+}
+
+int main() {
+    string s;
+    cout<<"Enter a string (with spaces): ";
+    cout << removeSpaces(s);
+    return 0;
+}
